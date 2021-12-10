287e7aa4b3c33f6985edcc01aa885a75efc07ead
Collins Zimba <dev@madebycollins.com>
2021-12-10 16:02:07 +0200

    Added useful information to the index page

--- moviefrontend/pages/index.vue
+++ moviefrontend/pages/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <Navbar/>
+  <div>
+    <p>
+    <h3>Movie app built using:</h3>
+    <br />
+    <ul>
+      <li>Vue.js // A frontend Javascript framework</li>
+      <li>Nuxt.js // A Vue.js framework</li>
+      <li>Bootstrap // A mobile first CSS framework</li>
+      <li>Axios // For making HTTP requests</li>
+    </ul>
+    </p>
+  </div>
 </template>
 
 <script>
