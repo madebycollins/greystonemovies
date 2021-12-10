b7531dce632a5f03b0c13708b391118759c0c9ae
Collins Zimba <dev@madebycollins.com>
2021-12-10 12:34:00 +0200

    Added navbar component

---
+++ moviefrontend/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">MoviesApp</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+
+        <b-navbar-nav class="ml-auto">
+          <b-navbar-nav>
+            <b-nav-item href="#">Home</b-nav-item>
+            <b-nav-item href="#">Movies</b-nav-item>
+          </b-navbar-nav>
+        </b-navbar-nav>
+
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+
+</style>
