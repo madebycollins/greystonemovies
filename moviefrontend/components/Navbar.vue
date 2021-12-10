06e5c29b07e85f3cc774924efe4f3c8fba712a64
Collins Zimba <dev@madebycollins.com>
2021-12-10 16:22:23 +0200

    Added links URLs to the Navbar

--- moviefrontend/components/Navbar.vue
+++ moviefrontend/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">MoviesApp</b-navbar-brand>
+      <b-navbar-brand href="/">MoviesApp</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
@@ -9,8 +9,8 @@
 
         <b-navbar-nav class="ml-auto">
           <b-navbar-nav>
-            <b-nav-item href="#">Home</b-nav-item>
-            <b-nav-item href="#">Movies</b-nav-item>
+            <b-nav-item href="/">Home</b-nav-item>
+            <b-nav-item href="/movies">Movies</b-nav-item>
           </b-navbar-nav>
         </b-navbar-nav>
 
