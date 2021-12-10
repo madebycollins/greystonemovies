46e6cd3187c75933505eca735f1f5797edb3e8f1
Collins Zimba <dev@madebycollins.com>
2021-12-10 16:11:23 +0200

    Improved html structure for index page

--- moviefrontend/pages/index.vue
+++ moviefrontend/pages/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <p>
     <h3>Movie app built using:</h3>
     <br />
-    <ul>
-      <li>Vue.js // A frontend Javascript framework</li>
-      <li>Nuxt.js // A Vue.js framework</li>
-      <li>Bootstrap // A mobile first CSS framework</li>
-      <li>Axios // For making HTTP requests</li>
-    </ul>
+    <p>
+      <ul>
+        <li>Vue.js // A frontend Javascript framework</li>
+        <li>Nuxt.js // A Vue.js framework</li>
+        <li>Bootstrap // A mobile first CSS framework</li>
+        <li>Axios // For making HTTP requests</li>
+      </ul>
     </p>
   </div>
 </template>
